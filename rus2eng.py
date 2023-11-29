ba23c56bf6da28637e804e6acd180e05ef1630c5
nlif-m <85732961+nlif-m@users.noreply.github.com>
2023-11-29 15:11:21 +0300

    Initial commit

---
+++ rus2eng.py
@@ -0,0 +1,18 @@
+import argparse
+import sys
+
+eng_qwerty = "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
+eng_qwerty += eng_qwerty.upper()
+ru_qwerty = "йцукенгшщзхъфывапролджэячсмитьбю."
+ru_qwerty += ru_qwerty.upper()
+
+rus2eng = {k: v for k, v in zip(ru_qwerty, eng_qwerty)}
+
+lines = []
+for line in sys.stdin:
+    for c in line:
+        append_char = c
+        if c in rus2eng:
+            append_char = rus2eng[c]
+        lines.append(append_char)
+print("".join(lines))
